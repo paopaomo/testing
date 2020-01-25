ef8e481ec9dfe4cce73c1a8c42c0e9aa0575db13
Zi Ye <18612319597@163.com>
2020-01-25 23:11:46 +0800

    [feature] benchmark

---
+++ test/benchmark.js
@@ -0,0 +1,42 @@
+const {num1, num2} = require('../src/fn');
+const Benchmark = require('benchmark');
+
+const suite = new Benchmark.Suite;
+
+// add tests
+// suite
+//     .add('RegExp#test', function () {
+//         /o/.test('Hello World!');
+//     })
+//     .add('String#indexOf', function () {
+//         'Hello World!'.indexOf('o') > -1;
+//     })
+//     .add('String#match', function () {
+//         !!'Hello World!'.match(/o/);
+//     })
+//     // add listeners
+//     .on('cycle', function (event) {
+//         console.log(String(event.target));
+//     })
+//     .on('complete', function () {
+//         console.log('Fastest is ' + this.filter('fastest').map('name'));
+//     })
+//     // run async
+//     .run({'async': true});
+
+// add our example
+
+suite
+    .add('parseInt', () => {
+        num1('123456');
+    })
+    .add('Number', () => {
+        num2('123456');
+    })
+    .on('cycle', (event) => {
+        console.log(String(event.target));
+    })
+    .on('complete', function() {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    .run({ async: true });
