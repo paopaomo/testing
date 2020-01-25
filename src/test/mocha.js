a0abb6701b2725735fb934ea266a28ec08c2a4e7
Zi Ye <18612319597@163.com>
2020-01-25 22:04:04 +0800

    [feature] test with mocha

---
+++ src/test/mocha.js
@@ -0,0 +1,18 @@
+const { should, expect, assert } = require('chai');
+const { add, multiple } = require('../math.js');
+
+describe('#math', () => {
+    describe('add', () => {
+        it('should return 5 when 2 + 3', () => {
+            expect(add(2, 3)).to.equal(5);
+        });
+        it('should return -1 when 2 + -3', () => {
+            expect(add(2, -3)).to.equal(-1);
+        });
+    });
+    describe('multiple', () => {
+        it('should return 6 when 2 * 3', () => {
+            expect(multiple(2, 3)).to.equal(6);
+        });
+    });
+});
